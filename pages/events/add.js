6eedf4107c0809ce6fe2aec6afc9be70d9e80657
Tommy Crafton <twcrafton@gmail.com>
2021-10-25 11:00:26 -0500

    Fix for webpack

--- pages/events/add.js
+++ pages/events/add.js
@@ -3,7 +3,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import Link from "next/Link";
+import Link from "next/link";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Form.module.css";
