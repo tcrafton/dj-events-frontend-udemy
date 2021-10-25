52ae4c5807c9b834c2d1cdb44f523fa8d8e09469
Tommy Crafton <twcrafton@gmail.com>
2021-10-25 10:10:44 -0500

    Updated

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,79 @@
-import Layout from "../../components/Layout";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
+import { useRouter } from "next/router";
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
+  const router = useRouter();
+
   return (
     <Layout>
-      <h1>My Event</h1>
+      <div className={styles.event}>
+        <span>
+          {new Date(evt.date).toLocaleDateString("en-US")} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        <ToastContainer />
+        {evt.image && (
+          <div className={styles.image}>
+            <Image
+              src={evt.image.formats.medium.url}
+              alt="Event Image"
+              width={960}
+              height={600}
+            />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.bake}>{"<"} Back</a>
+        </Link>
+      </div>
     </Layout>
   );
+}
+
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/events`);
+//   const events = await res.json();
+
+//   const paths = events.map((evt) => ({
+//     params: { slug: evt.slug },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// }
+
+// export async function getStaticProps({ params: { slug } }) {
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: { evt: events[0] },
+//     revalidate: 1,
+//   };
+// }
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+  const events = await res.json();
+
+  return {
+    props: { evt: events[0] },
+  };
 }
